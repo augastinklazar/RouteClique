c83070e05eeb44f08968206fa33bf139a387355e
Augastin K Lazar <82433581+augastinklazar8673@users.noreply.github.com>
2021-07-12 22:15:53 +0530

    add Database php file

--- connect/DB.php
+++ connect/DB.php
@@ -1,1 +1,25 @@
 
+
+<?php
+
+class DB{
+
+    private static function connect(){
+        $pdo = new PDO('mysql:host=127.0.0.1; dbname=Routeclique; charset=utf8mb4', 'root', '');
+        // PDO-php data objects... provides a data-access abstraction layer. i.e. regardless of the database used.... u use same fns to issue queries and fetch data.
+
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    }//can only be accessed from this class
+
+    public static function query($query, $params = array()){
+        $statement = self::connect()->prepare($query);
+        $statement->execute($params);
+
+        if(explode(' ', $query)[0] == 'SELECT'){
+            $data = $statement->fetchAll();
+            return $data;
+        }
+    }
+}
+?>
