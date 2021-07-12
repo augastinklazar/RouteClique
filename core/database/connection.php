bb54095ccacad8553daa63c0e3c775ce999e5b8c
Augastin K Lazar <82433581+augastinklazar8673@users.noreply.github.com>
2021-07-12 22:20:44 +0530

    Add files via upload

--- core/database/connection.php
+++ core/database/connection.php
@@ -1,1 +1,18 @@
+<?php
 
+$hostdetails = 'mysql:host=127.0.0.1; dbname=RouteClique; charset=utf8mb4';
+$useradmin = 'root';
+$pass = '';
+
+try{
+    $pdo = new PDO($hostdetails, $useradmin, $pass);
+}
+catch(PDOException $e) {
+    echo 'Connection error!!!'.$e->getMessage();
+}
+
+
+
+
+
+?>
